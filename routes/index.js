ea9d23c9e2a0205aff71d8f4a383e4533ed001f1
chester <chester0148@gmail.com>
2018-09-30 20:55:31 +0800

    setting layout

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/index', { 
-    title: 'Express' , 
+    title: '首頁' , 
     content: '這很實用',
-    body: req.body
+    body: req.body,
+
   });
 });
 
