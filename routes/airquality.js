36ed00da9a3b7948481fc76933d183a76d387973
chester <chester0148@gmail.com>
2018-10-14 23:20:56 +0800

    test update

--- routes/airquality.js
+++ routes/airquality.js
@@ -1,22 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-// var bodyParser = require('body-parser');
-
-// var app = express();
-// var server = require('http').createServer(app);
-
-// app.use(bodyParser.json({limit: '10mb'}));  //body-parser 解析json格式数据
-// app.use(bodyParser.urlencoded({            //此项必须在 bodyParser.json 下面,为参数编码
-//   extended: true
-// }));
-
 const https = require("http");
 let jsonstr = "";
 const url = "http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json";
+
 https.get(url, res => {
   res.setEncoding("utf8");
-  
   res.on("data", data => {
     jsonstr += data;
   });
@@ -24,6 +14,7 @@ https.get(url, res => {
     jsonstr = JSON.parse(jsonstr);
   });
 });
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
